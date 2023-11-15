460ddf8d092ee4a7f02d8c62e7db25b6631704f6
root <14608193@qq.com>
2023-11-15 19:44:25 +0800

    修改错误类型

--- myfront/src/error.rs
+++ myfront/src/error.rs
@@ -1,10 +1,10 @@
-use std::fmt::Debug;
+use std::fmt::{Debug, Display};
 
 use tracing::error;
 
 /// 自定义错误类型
 #[derive(thiserror::Error, Debug)]
 pub enum FrontError {
-    #[error("未知的服务器内部错误:{0:?}")]
-    UnknownError(anyhow::Error),
+    #[error("未知错误:{0:?}")]
+    UnknownError(String),
 }
