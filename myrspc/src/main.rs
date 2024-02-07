11630fc988380829ccf9f7375335b54196898322
root <14608193@qq.com>
2024-02-07 20:21:43 +0800

    rspc delete

--- myrspc/src/main.rs
+++
@@ -1,8 +0,0 @@
-use rspc::Router;
-
-#[tokio::main]
-async fn main() {
-    let router = <Router>::new()
-        .query("version", |t| t(|ctx, input: ()| env!("CARGO_PKG_VERSION")))
-        .build();
-}
