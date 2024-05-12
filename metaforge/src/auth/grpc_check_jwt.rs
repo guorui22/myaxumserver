f2807c493642ca146c58e796ee5b41b97a03947a
root <14608193@qq.com>
2024-05-12 17:43:36 +0800

    故意构造 grpc 服务校验 JWT 失败

--- metaforge/src/auth/grpc_check_jwt.rs
+++ metaforge/src/auth/grpc_check_jwt.rs
@@ -5,11 +5,15 @@ use crate::model::global_const::JWT;
 pub fn grpc_check_auth(req: Request<()>) -> Result<Request<()>, Status> {
     match req.metadata().get("authorization") {
         Some(t) if {
-            let jwt = t.to_str().map_err(|_|Status::unauthenticated("No valid auth token"))?.split_whitespace().collect::<Vec<&str>>()[1];
-            let claims = JWT.verify_and_get(jwt).unwrap();
-            !claims.is_expired()
+            let jwt = t.to_str().map_err(|_|Status::unauthenticated("不是有效的 JWT Token。"))?.split_whitespace().collect::<Vec<&str>>()[1];
+            let claims = JWT.verify_and_get(jwt).map_err(|_|Status::unauthenticated("JWT Token 验证失败。"))?;
+            return if claims.is_expired() {
+                Err(Status::unauthenticated("JWT Token 已过期。"))
+            } else {
+                Err(Status::unauthenticated("JWT Token 已过期。"))
+            }
         } => Ok(req),
-        _ => Err(Status::unauthenticated("No valid auth token")),
+        _ => Err(Status::unauthenticated("请求头中没有 authorization 字段。")),
     }
 }
 
