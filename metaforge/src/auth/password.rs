439e9af63ed7233ef6d0bef1e443f37b64acf70d
root <14608193@qq.com>
2024-06-04 16:26:10 +0800

    aes加密解密

--- metaforge/src/auth/password.rs
+++ metaforge/src/auth/password.rs
@@ -1,6 +1,53 @@
+/// 为字符串生成哈希字符串
 pub fn hash(pwd: &str) -> Result<String, String> {
     bcrypt::hash(pwd, bcrypt::DEFAULT_COST).map_err(|err| err.to_string())
 }
+
+/// 验证密码是否匹配哈希字符串
 pub fn verify(pwd: &str, hashed_pwd: &str) -> Result<bool, String> {
     bcrypt::verify(pwd, hashed_pwd).map_err(|err| err.to_string())
+}
+
+/// AES 加密
+pub fn aes_encrypt(input: String) -> String {
+
+    // 引入加密算法库
+    use aes::cipher::{KeyIvInit, StreamCipher};
+
+    // 定义类型别名
+    type Aes128Ctr64LE = ctr::Ctr64LE<aes::Aes128>;
+
+    // 密钥、IV
+    let key = [1, 3, 5, 8, 1, 9, 0, 7, 4, 0, 6, 12, 13, 14, 15, 16];
+    let iv = [0x12; 16];
+    let mut buf = input.into_bytes();
+
+    // 加密消息
+    let mut cipher = Aes128Ctr64LE::new(&key.into(), &iv.into());
+    cipher.apply_keystream(&mut buf);
+
+    // 返回密文
+    const_hex::encode(&buf)
+
+}
+
+/// AES 解密
+pub fn aes_decrypt(input: String) -> String {
+    // 引入加密算法库
+    use aes::cipher::{KeyIvInit, StreamCipher};
+
+    // 定义类型别名
+    type Aes128Ctr64LE = ctr::Ctr64LE<aes::Aes128>;
+
+    // 密钥、IV
+    let key = [1, 3, 5, 8, 1, 9, 0, 7, 4, 0, 6, 12, 13, 14, 15, 16];
+    let iv = [0x12; 16];
+    let mut buf = const_hex::decode(input).unwrap();
+
+    // 解密消息
+    let mut cipher = Aes128Ctr64LE::new(&key.into(), &iv.into());
+    cipher.apply_keystream(&mut buf);
+
+    // 返回明文
+    unsafe { String::from_utf8_unchecked(buf) }
 }
--- metaforge/src/auth/test.rs
+++ metaforge/src/auth/test.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod test {
-    use libproto::Input;
     use crate::auth::JwtSecret;
+    use crate::auth::password::*;
 
     const SECRET: &str = "不负信赖";
     const ISS: &str = "圣农集团";
@@ -12,7 +12,7 @@ mod test {
         let jwt = JwtSecret::new(SECRET.to_string(), ISS.to_string());
         let claims = jwt.create_claims("1".to_string(), "team@axum.rs".to_string(), 30);
         let token = jwt.to_token(&claims).unwrap();
-        println!("{:?}", &token);
+        dbg!(token);
     }
 
     /// 测试解析 token 信息为 claims
@@ -22,19 +22,20 @@ mod test {
         let claims = jwt.create_claims("1".to_string(), "team@axum.rs".to_string(), 30);
         let token = jwt.to_token(&claims).unwrap();
         let claims = jwt.verify_and_get(token.as_str()).unwrap();
-        println!("{}", claims);
+        dbg!(claims);
     }
 
     /// 测试密码哈希和验证
     #[test]
     fn test_pwd_hash_verify() {
         let pwd = "123456";
-        let hashed = crate::auth::password::hash(pwd).unwrap();
+        let hashed = hash(pwd).unwrap();
         dbg!(format!("hashed: {}", hashed));
-        let verified = crate::auth::password::verify(pwd, &hashed).unwrap();
+        let verified = verify(pwd, &hashed).unwrap();
         assert!(verified);
     }
 
+    /// 测试 AES 加密和解密步骤
     #[test]
     fn test_pwd_aes() {
         use aes::cipher::{KeyIvInit, StreamCipher, StreamCipherSeek};
@@ -52,8 +53,8 @@ mod test {
         let mut buf = plaintext.to_vec();
         let mut cipher = Aes128Ctr64LE::new(&key.into(), &iv.into());
         cipher.apply_keystream(&mut buf);
-        dbg!(format!("buf: {}", u8_vec_to_hex_string(buf.clone())));
-        dbg!(format!("ciphertext: {}", u8_vec_to_hex_string(ciphertext.to_vec())));
+        dbg!(format!("buf: {}", const_hex::encode(buf.clone())));
+        dbg!(format!("ciphertext: {}", const_hex::encode(ciphertext.to_vec())));
         assert_eq!(buf[..], ciphertext[..]);
 
         // CTR 模式可用于流式消息解密
@@ -71,8 +72,8 @@ mod test {
         // 输出缓冲区长度必须等于输入缓冲区长度
         let mut buf1 = vec![0u8; plaintext.len()].into_boxed_slice();
         cipher.apply_keystream_b2b(&plaintext, &mut buf1).unwrap();
-        dbg!(format!("buf1: {}", u8_vec_to_hex_string(buf1.to_vec())));
-        dbg!(format!("ciphertext: {}", u8_vec_to_hex_string(ciphertext.to_vec())));
+        dbg!(format!("buf1: {}", const_hex::encode(buf1.to_vec())));
+        dbg!(format!("ciphertext: {}", const_hex::encode(ciphertext.to_vec())));
         assert_eq!(buf1[..], ciphertext[..]);
 
         // CTR 模式支持加密计数器重置从 0 开始
@@ -86,48 +87,7 @@ mod test {
         assert_eq!(buf2[..], plaintext[..]);
     }
 
-    fn aes_encrypt(input: String) -> String {
-
-        // 引入加密算法库
-        use aes::cipher::{KeyIvInit, StreamCipher, StreamCipherSeek};
-
-        // 定义类型别名
-        type Aes128Ctr64LE = ctr::Ctr64LE<aes::Aes128>;
-
-        // 密钥、IV、明文、密文
-        let key = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-        let iv = [0x0; 16];
-        let mut buf = input.into_bytes();
-
-        // 加密消息
-        let mut cipher = Aes128Ctr64LE::new(&key.into(), &iv.into());
-        cipher.apply_keystream(&mut buf);
-
-        // 返回密文
-        const_hex::encode(&buf)
-
-    }
-
-    fn aes_decrypt(input: String) -> String {
-        // 引入加密算法库
-        use aes::cipher::{KeyIvInit, StreamCipher, StreamCipherSeek};
-
-        // 定义类型别名
-        type Aes128Ctr64LE = ctr::Ctr64LE<aes::Aes128>;
-
-        // 密钥、IV、明文、密文
-        let key = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-        let iv = [0x0; 16];
-        let mut buf = const_hex::decode(input).unwrap();
-
-        // 解密消息
-        let mut cipher = Aes128Ctr64LE::new(&key.into(), &iv.into());
-        cipher.apply_keystream(&mut buf);
-
-        // 返回明文
-        unsafe { String::from_utf8_unchecked(buf) }
-    }
-
+    /// 测试 AES 加密和解密函数
     #[test]
     fn test_aes_encrypt_decrypt() {
         let input = "123456";
@@ -138,11 +98,4 @@ mod test {
         assert_eq!(input, plaintext);
     }
 
-    // helper 函数，将 Vec<u8> 转换为 hex 字符串
-    fn u8_vec_to_hex_string(vec: Vec<u8>) -> String {
-        vec.iter()
-            .map(|byte| format!("{:02x}", byte))
-            .collect::<Vec<String>>()
-            .join("")
-    }
 }
