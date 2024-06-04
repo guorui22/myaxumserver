6ee5dc8ccbdc2a21a9d6caf8e143f5b9293f74e5
root <14608193@qq.com>
2024-06-04 18:35:55 +0800

    添加访问数据库用户表功能

--- metaforge/src/grpc_server/login_server.rs
+++ metaforge/src/grpc_server/login_server.rs
@@ -1,17 +1,19 @@
+use axum::Json;
 use libproto::login_service_server::LoginService;
 use libproto::{LoginReply, LoginReplyData, LoginRequest};
 use tonic::{Request, Response, Status};
-use crate::auth::JwtSecret;
+use libdatabase::{GrMySQLPool, TestMySqlDb01};
+use crate::auth::{aes_encrypt, JwtSecret};
 
 #[derive(Clone, Debug)]
-pub struct Login<T: Clone + Send + Sync + 'static> {
+pub struct Login {
     pub jwt: JwtSecret,
     pub jwt_exp: i64,
-    pub db_pool: T,
+    pub db_pool: GrMySQLPool<TestMySqlDb01>,
 }
 
 #[tonic::async_trait]
-impl<T: Clone + Send + Sync + 'static> LoginService for Login<T> {
+impl LoginService for Login {
     async fn do_login(
         &self,
         request: Request<LoginRequest>,
@@ -21,6 +23,17 @@ impl<T: Clone + Send + Sync + 'static> LoginService for Login<T> {
         let password = input.password;
 
         dbg!(format!("usercode: {}, password: {}", usercode, password));
+
+        let user = libdatabase::mysql_query(self.db_pool.clone(), vec![format!("select * from sys_user where user_code = '{}' and user_password = '{}'", usercode, aes_encrypt(password))])
+            .await
+            .map_err(|err| Status::internal(format!("数据库查询失败: {}", err)))
+            .map(Json)?;
+        let status = Status::internal("数据库查询结果解析失败。".to_string());
+        dbg!(format!("user: {:?}", user.as_object().ok_or(status.clone())?
+            .get("result").ok_or(status.clone())?
+            .as_array().ok_or(status.clone())?
+            .get(0).ok_or(status.clone())?));
+        dbg!(format!("user: {:?}", user));
 
         let jwt = &self.jwt;
         let claims = jwt.create_claims(usercode.clone(), "郭睿".to_string(), self.jwt_exp);
